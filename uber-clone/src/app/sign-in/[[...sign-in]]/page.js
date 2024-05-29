e4bfecfdf9028d6a4064031781fdcc2fdcd72795
yeabsira <yeabiramulualem@gmail.com>
2024-05-29 09:19:51 -0600

    sign in and sign up page created using clerk auth

---
+++ uber-clone/src/app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,16 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <>
+    <Image src='/uberbanner.jpg' width={900} height={1000}
+       className="object-contain h-full w-full"/>
+    <div className="absolute top-20 right-10">
+       
+    <SignIn />
+    </div>
+    </>
+  
+);
+}
